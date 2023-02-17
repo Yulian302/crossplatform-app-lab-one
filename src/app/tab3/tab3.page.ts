d912ab4b8418928a6434e9353343229542e4b6a2
Yulian302 <bohomolyulian3022003@gmail.com>
2023-02-17 16:05:03 +0200

    у

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,3 @@
-import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,35 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
   constructor() {
-    this.n=0;
-    this.min=this.max=this.matrix[0][0];
+    this.n = 0;
+    this.min = 0;
+    this.max = 0;
   }
   matrix: number[][] = [];
-  n:number;
-  min:number;
-  max:number;
-  generateMatrix(n:string|undefined|number|null)
-  {
-    this.n=Number(n);
-    this.matrix=Array(this.n);
-    let i:number,j:number=0;
-    for (i =0; i < this.n;i++) {
-      for (j = 0; j < this.n; j++) {
+  n: any;
+  min: number;
+  max: number;
+  generateMatrix(n: string | undefined | number | null) {
+    this.n = Number(n);
+    this.matrix = Array(this.n);
+    let i: number=0, j: number = 0;
+    for (i = 0; i < this.n; i++) {
+      this.matrix[i]=Array(this.n);
+      for(j=0;j<this.n;j++){
         this.matrix[i][j]=this.generateRandom(1,50);
       }
     }
     this.getMaxAndMin();
   }
-  generateRandom(min:number,max:number)
-  {
+  generateRandom(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   getMaxAndMin()
   {
     let i:number=0;
     let j:number=0;
+    this.max=this.matrix[0][0];
+    this.min=this.matrix[0][0];
     for(i=0;i<this.n;i++)
     {
       for(j=0;j<this.n;j++)
@@ -55,4 +55,5 @@ export class Tab3Page {
   {
     return b==this.min?'red':b==this.max?'green':'none';
   }
+  ngOnInit(){}
 }
